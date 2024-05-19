b84952dc93b8d1240c6a20353f77706039cf546d
Rusty <81254685+RustyDeGarmo@users.noreply.github.com>
2024-05-18 19:19:03 -0500

    minor changes to player collision shape to clean that up a bit

--- Godot Crash Course Platformer/Player/Player.gd
+++ Godot Crash Course Platformer/Player/Player.gd
@@ -21,6 +21,11 @@ func _physics_process(delta):
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction = Input.get_axis("ui_left", "ui_right")
+	if direction == -1:
+		get_node("AnimatedSprite2D").flip_h = true
+	elif direction == 1:
+		get_node("AnimatedSprite2D").flip_h = false
+		
 	if direction:
 		velocity.x = direction * SPEED
 		if velocity.y == 0:
@@ -29,6 +34,7 @@ func _physics_process(delta):
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 		if velocity.y == 0:
 			anim.play("Idle")
+	
 	if velocity.y > 0:
 		anim.play("Fall")
 
